f6ddd50b5457352e71ab11be9a3e39e3f9c28791
snag <somjitnag192@gmail.com>
2021-08-02 07:40:56 +0530

    loadbalancer 101 working

---
+++ Udemy/node_app/server.js
@@ -0,0 +1,33 @@
+// A simple no framework node.js server
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(`Server:${PORT} :: req :: ${req.url}`)
+    if (req.url === '/' && req.method === 'GET') {
+        res.end(`${PORT} :: Gettings Root!`);
+        
+    } else if (req.url === '/greet' && req.method === 'GET') {
+        res.end(`${PORT} :: Hello User!`);
+
+    } else if ((req.url.match(/\/greet\/\w+/)) && req.method === 'GET') {
+        const name = req.url.split('/')[2]
+        res.end(`${PORT} :: Gettings ${name}!`);
+
+    } else if (req.url.match(/\/products\/\w+/) && req.method === 'GET') {
+        const id = req.url.split('/')[2]
+        res.end(`${PORT} :: Getting Product#${id}`);
+
+    } else if (req.url === '/products' && req.method === 'GET') {
+        res.end(`${PORT} :: \n${JSON.stringify(["Apple", "Orange"])}`);
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(`${PORT} :: \n${JSON.stringify({ message: 'Route Not Found' })}`)
+
+    }
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+module.exports = server;
